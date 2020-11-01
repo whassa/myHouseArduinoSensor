8c97cc3004a6d841626bc7b7a2f793ae07f19b07
whassa <remi.graton@gmail.com>
2020-11-01 17:26:11 -0500

    Some fixing

--- main/main.ino
+++ main/main.ino
@@ -1,20 +1,38 @@
 /*
   My light house welcome project
 */
+#include <SimpleSDAudio.h>
 
 byte soundPlayed;
+int Capteur= 2;
+char sound[] ="swag.wav";
 
 void setup() {
   // initialize stuff and shizzle
-  pinMode(12, OUTPUT);
-  pinMode(10, INPUT);
+  SdPlay.setSDCSPin(4); 
+  
+  pinMode(Capteur,INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9601);
+  Serial.println("salut");
+
+  if (!SdPlay.init(SSDA_MODE_FULLRATE | SSDA_MODE_MONO | SSDA_MODE_AUTOWORKER)){ 
+    while(1);
+   }
+
+  if(!SdPlay.setFile(sound)) // music name file
+  { 
+    while(1);
+  } 
 }
 
-// LOOOOP
 void loop() {
-  if( digitalRead(10) ){
-      tone(12, 3000);
-  } else {
-      noTone(12);
-  }
+
+   SdPlay.play(); // play music
+
+  while(!SdPlay.isStopped())
+  {
+  
+  } 
+  
 }
